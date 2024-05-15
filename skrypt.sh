42ddbe6299a1c3b5857fb1f6da3ae1d512051f07
JakubHaran1 <jakub.haran02@gmail.com>
2024-05-15 23:04:14 +0200

    create 100logs

--- skrypt.sh
+++ skrypt.sh
@@ -4,3 +4,12 @@ if [ "$1" = "--date" ]; then
 	echo "Data: $date"
 fi
 echo "*log*" > .gitignore
+
+if [ "$1" = "--logs" ]; then
+	for (( i=1; i<=100; i++ )); do
+		file_name="log${i}.txt"
+		echo "Nazwa pliku: $file_name" > "$file_name"
+		echo "Nazwa skryptu: $0" >> "file_name"
+		echo "Data kiedy został utworzony plik $(date)" >> "file_name"
+	done
+fi
